bb442bdc861f26853b5158caced6490918746ebe
Alioth0421 <1427720979@qq.com>
2020-06-23 19:46:48 +0800

    sixth homework

---
+++ lessons6/led.c.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(1,1);
+  lcd.print("hello, world!");
+  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.begin(16, 2);
+  lcd.print("Let's go!");
+  Serial.begin(9600);
+}
+
+void loop() {
+  //lcd.setCursor(0, 1);
+  //lcd.print("hello,SN");
+  if(Serial.available()>0){
+  lcd.setCursor(0,1);
+  lcd.print(Serial.readString());
+  }
+}
+
